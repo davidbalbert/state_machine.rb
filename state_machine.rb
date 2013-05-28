bdd5678a8d415c3066ec974da85d14b3bd5b5168
David Albert <davidbalbert@gmail.com>
2013-05-28 15:22:31 -0400

    Remove example from state_machine.rb

--- state_machine.rb
+++ state_machine.rb
@@ -84,33 +84,3 @@ module StateMachine
     @state = state
   end
 end
-
-if __FILE__ == $0
-  class FooMachine
-    include StateMachine
-
-    start_state :foo
-
-    define_transition :foo => :bar, :bar => :baz do |from, to|
-      puts "#{from.inspect} to #{to.inspect}"
-    end
-
-    define_transition :baz => :qux do
-      puts @ivar
-    end
-
-    define_transition :qux => :bozo do
-      42
-    end
-
-    def initialize
-      @ivar = "hi there"
-    end
-  end
-
-  fm = FooMachine.new
-  fm.transition_to(:bar)
-  fm.transition_to(:baz)
-  fm.transition_to(:qux)
-  puts fm.transition_to(:bozo)
-end
